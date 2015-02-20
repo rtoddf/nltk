02706d4033340cbc792ca6d9c07d2bf3912050e8
Todd Fleeman <rtoddf@comcast.net>
2015-02-20 10:47:42 -0500

    fixing stuff

--- views.py
+++ views.py
@@ -3,11 +3,11 @@ from flask import render_template
 
 from app import app
 
-import nltk
+# import nltk
 from nltk.corpus import gutenberg
 from nltk.corpus import brown
-# from nltk.corpus import reuters
-# from nltk.corpus import inaugural
+from nltk.corpus import reuters
+from nltk.corpus import inaugural
 from nltk.corpus import webtext as webtext
 
 # import nltk.book as book
@@ -21,31 +21,31 @@ from helpers import get_lexical_diversity, get_fileids, get_categories, get_raw_
 @app.route('/')
 def home():
     inaugural_file_ids = get_fileids(inaugural)
-    speech = inaugural_file_ids[-5]
-
+    speech = inaugural_file_ids[-1]
+    print inaugural_file_ids
     
-    gutenberg_file_ids = get_fileids(gutenberg)
-    brown_file_ids = get_fileids(brown)
-    brown_categories = get_categories(brown)
-    # reuters_categories = get_categories(reuters)
+    # gutenberg_file_ids = get_fileids(gutenberg)
+    # brown_file_ids = get_fileids(brown)
+    # brown_categories = get_categories(brown)
+    # # reuters_categories = get_categories(reuters)
     
     raw_text = get_raw_text(inaugural, speech)
     text_count = get_text_count(inaugural, speech)
     uniqs_count = get_uniqs_count(inaugural, speech)
     uniqs = get_uniqs(inaugural, speech)
 
-    print inaugural_file_ids[-1]
-
     return render_template('index.html',
-        gutenberg_file_ids=gutenberg_file_ids,
-        brown_file_ids=brown_file_ids,
-        brown_categories=brown_categories,
-        # reuters_categories=reuters_categories,
         inaugural_file_ids=inaugural_file_ids,
         raw_text=raw_text,
         text_count=text_count,
         uniqs_count=uniqs_count,
         uniqs=uniqs)
+
+    # gutenberg_file_ids=gutenberg_file_ids,
+    #     brown_file_ids=brown_file_ids,
+    #     brown_categories=brown_categories,
+    #     # reuters_categories=reuters_categories,
+        
 
 @app.route('/macbeth')
 def words():
